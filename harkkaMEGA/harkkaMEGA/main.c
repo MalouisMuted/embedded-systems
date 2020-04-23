26fae80a701501aacc9be303200c064863801a07
ekosfin <elias.samuli@gmail.com>
2020-04-23 15:00:50 +0300

    lisätty harkkaMEGA

---
+++ harkkaMEGA/harkkaMEGA/main.c
@@ -0,0 +1,18 @@
+/*
+ * harkkaMEGA.c
+ *
+ * Created: 23.4.2020 14.59.55
+ * Author : elias
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
