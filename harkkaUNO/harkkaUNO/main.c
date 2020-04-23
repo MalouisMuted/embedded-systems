80444311eb291cc4ee3a0b744a944b28a17a21db
ekosfin <elias.samuli@gmail.com>
2020-04-23 14:59:01 +0300

    harkkaUNO projekti aloitettu

---
+++ harkkaUNO/harkkaUNO/main.c
@@ -0,0 +1,18 @@
+/*
+ * harkkaUNO.c
+ *
+ * Created: 23.4.2020 14.58.40
+ * Author : elias
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
