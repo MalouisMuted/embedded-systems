f1719321f3b409d564f28a0b41729b08e789d57a
ekosfin <elias.samuli@gmail.com>
2020-04-24 13:14:09 +0300

    refined SPI slave and added motion sensor check

--- harkkaUNO/harkkaUNO/main.c
+++ harkkaUNO/harkkaUNO/main.c
@@ -11,20 +11,55 @@ void SPI_SlaveInit(void)
     /* Enable SPI */
     SPCR = (1 << SPE);
 }
-char SPI_SlaveReceive(void)
+char SPI_SlaveReceive(char spi_send_data[20])
 {
-    /* Wait for reception complete */
-    while (!(SPSR & (1 << SPIF)))
-        ;
+	char spi_receive_data[20];
+	/* alkaa menemään lähetettävää viestiä läpi bittibitiltä */
+	for(int8_t spi_data_index = 0; spi_data_index < sizeof(spi_send_data); spi_data_index++){
+		/* lataa send bufferiin lähetettävän bitin*/
+		SPDR = spi_send_data[spi_data_index];
+		while (!(SPSR & (1 << SPIF))){
+			; /* odottaa että saa luvan lähettää ja samalla vastaanottaa bitin */
+		}
+		spi_receive_data[spi_data_index] = SPDR; /* tallettaa saatua dataa bitti bitiltä*/
+	}
     /* Return Data Register */
-    return SPDR;
+    return spi_receive_data;
+}
+void MotionSensorInit(void)
+{
+	/* Set pin7(PD7) as input for motion sensor */
+	DDRD &= ~(1 << PD7);
+	
+}
+char MotionSensorCheck(void)
+{
+	int8_t motion_value = 0;
+	char status[20];
+	/* checking if there has been motion */
+	motion_value = (PIND & (1<<PD7));
+	/* returning dependent on value */
+	if (0 != motion_value) {
+		status = "motion";
+	} else {
+		status = "still";
+	}
+	return status;
 }
 
 int main(void)
 {
     // enable global interrupts
-    // sei();
+	char send[20];
+	char recive[20];
+	SPI_SlaveInit();
+	MotionSensorInit();
+	// Loop
     while (1)
     {
+		/* Check sensor and then transmit to SPI master */
+		send = MotionSensorCheck();
+		recive = SPI_SlaveReceive(send);
     }
+	return 0;
 }
