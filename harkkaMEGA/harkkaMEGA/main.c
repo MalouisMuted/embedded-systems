e218c9957ab2cd29f124fe4a24f681a18b34e157
ekosfin <elias.samuli@gmail.com>
2020-05-03 14:57:35 +0300

    added SPI and UART for mega

--- harkkaMEGA/harkkaMEGA/main.c
+++ harkkaMEGA/harkkaMEGA/main.c
@@ -11,12 +11,64 @@
 
 #define F_CPU 16000000UL
 
+//UART for debugging
+#define FOSC 16000000UL // Clock Speed
+#define BAUD 9600
+#define MYUBRR (FOSC/16/BAUD-1)
+//--------------
 
 #include <avr/io.h>
+#include <stdlib.h>
 #include "lcd.h" // LCD Library by Peter Fleury.
-#include "keypad.h"
+#include "keypad.h" // Keypad Library by https://www.exploreembedded.com/wiki/AVR_C_Library
 
+//UART for debugging 
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <util/setbaud.h>
+#include <stdio.h>
 
+static void USART_init(uint16_t ubrr)
+{
+	/* Set baud rate in the USART Baud Rate Registers (UBRR) */
+	UBRR0H = (unsigned char) (ubrr >> 8);
+	UBRR0L = (unsigned char) ubrr;
+	
+	/* Enable receiver and transmitter on RX0 and TX0 */
+	UCSR0B |= (1 << RXEN0) | (1 << TXEN0);
+	
+	/* Set frame format: 8 bit data, 2 stop bit */
+	UCSR0C |= (1 << USBS0) | (3 << UCSZ00);
+	
+}
+
+static void USART_Transmit(unsigned char data, FILE *stream)
+{
+	/* Wait until the transmit buffer is empty*/
+	while(!(UCSR0A & (1 << UDRE0)))
+	{
+		;
+	}
+	
+	/* Puts the data into a buffer, then sends/transmits the data */
+	UDR0 = data;
+}
+
+static char USART_Receive(FILE *stream)
+{
+	/* Wait until the transmit buffer is empty*/
+	while(!(UCSR0A & (1 << UDRE0)))
+	{
+		;
+	}
+	
+	/* Get the received data from the buffer */
+	return UDR0;
+}
+// Setup the stream functions for UART
+FILE uart_output = FDEV_SETUP_STREAM(USART_Transmit, NULL, _FDEV_SETUP_WRITE);
+FILE uart_input = FDEV_SETUP_STREAM(NULL, USART_Receive, _FDEV_SETUP_READ);
+// ----------------------------------
 
 const char message1_connection_established[] = "Alarm connected successfully";
 const char message2_movement_detected[] = "Movement detected";
@@ -28,12 +80,22 @@ int8_t g_state = 0;
 
 /* Fucntion declarations */
 void display_message(int message_number, int password_length); 
+void SPI_init();
+char *SPI_communicate();
 
 int main(void)
 {
-	// LCD and keypad initialization.
+	// LCD, SPI and Keypad initialization.
 	lcd_init(LCD_DISP_ON);
 	KEYPAD_Init();
+	SPI_init();
+	
+	// INIT of UART
+	USART_init(MYUBRR);
+	stdout = &uart_output;
+	stdin = &uart_input;
+	//----------------
+	
     /* Replace with your application code */
     while (1) 
     {
@@ -105,3 +167,43 @@ void display_message(int message_number, int password_length)
 
 }
 
+/* initialization of SPI communication by setting ports */
+void SPI_init()
+{
+	/* set SS, MOSI and SCK as output, pins 53 (PB0), 51 (PB2) and 52 (PB1) */
+	DDRB |= (1 << PB0) | (1 << PB1) | (1 << PB2); // SS as output
+	/* set SPI enable and master/slave select, making MEGA the master */
+	SPCR |= (1 << 6) | (1 << 4);
+	/* set SPI clock rate to 1 MHz */
+	SPCR |= (1 << 0);
+}
+
+/* SPI communication master to slave */
+char *SPI_communicate()
+{
+	char spi_send_data[20] = "master to slave\n";
+	char* spi_receive_data = malloc(20);
+	/* send byte to slave and receive a byte from slave */
+	PORTB &= ~(1 << PB0); // SS LOW
+	for(int8_t spi_data_index = 0; spi_data_index < sizeof(spi_send_data); spi_data_index++)
+	{
+		
+		SPDR = spi_send_data[spi_data_index]; // send byte using SPI data register
+		
+		while(!(SPSR & (1 << SPIF)))
+		{
+			/* wait until the transmission is complete */
+			;
+		}
+		
+		spi_receive_data[spi_data_index] = SPDR; // receive byte from the SPI data register
+	}
+	/* End communication */
+	PORTB |= (1 << PB0); // SS HIGH
+	
+	//Debugging sending received message to UART
+	printf(spi_receive_data);
+	
+	return spi_receive_data;
+}
+	
